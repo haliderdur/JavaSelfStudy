eb9158ed87eb66bef2a55878cf8ab10fa77f40fc
Halid Erdur <haliderdur@gmail.com>
2022-12-06 18:51:42 +0000

    Deleted redundant code fragments

--- src/Replit/Replit_ArrayList/CombineArrays.java
+++ src/Replit/Replit_ArrayList/CombineArrays.java
@@ -5,17 +5,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CombineArrays {
-    
+
     public static ArrayList<String> combine(String[] r1, String[] r2) {
+
         ArrayList<String> result = new ArrayList<>(r1.length + r2.length);
         result.addAll(Arrays.asList(r1));
         result.addAll(Arrays.asList(r2));
 
         return result;
-
     }
-    
-    // halid
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -27,7 +25,6 @@ public class CombineArrays {
         for (int i = 0; i < strs2.length; i++) {
             strs2[i] = in.next();
         }
-
         System.out.println(combine(strs, strs2));
     }
 }
