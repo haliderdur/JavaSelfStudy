3433834fb6e79480e3e765adf87d0ec48fef4c30
Halid Erdur <haliderdur@gmail.com>
2024-05-30 19:55:31 +0100

    practise

---
+++ src/CodingBat/Warmup1/missingChar.java
@@ -0,0 +1,29 @@
+package CodingBat.Warmup1;
+
+public class missingChar {
+
+    /*
+        Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+        The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+        missingChar("kitten", 1) → "ktten"
+        missingChar("kitten", 0) → "itten"
+        missingChar("kitten", 4) → "kittn"
+     */
+
+    public String missingChar(String str, int n) {
+
+        if (n >= str.length()) {
+            System.err.println("'n' can not be longer than given string");
+            System.exit(1);
+        }
+        String front = str.substring(0, n);
+        String back = str.substring(n + 1, str.length());
+        return front + back;
+    }
+
+    public static void main(String[] args) {
+        missingChar mc = new missingChar();
+        System.out.println(mc.missingChar("Halid", 12));
+    }
+}
---
+++ src/CodingBat/Warmup1/notString.java
@@ -0,0 +1,33 @@
+package CodingBat.Warmup1;
+
+import java.util.Scanner;
+
+public class notString {
+
+    /*
+        Given a string, return a new string where "not " has been added to the front.
+        However, if the string already begins with "not", return the string unchanged.
+        Note: use .equals() to compare 2 strings.
+
+
+        notString("candy") → "not candy"
+        notString("x") → "not x"
+        notString("not bad") → "not bad"
+     */
+
+    public String notString(String str) {
+
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+        return "not " + str;
+
+    }
+
+
+    public static void main(String[] args) {
+        notString obj = new notString();
+        System.out.println(obj.notString("x"));
+    }
+
+}
