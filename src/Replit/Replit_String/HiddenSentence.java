82163a48d2806ac1fdde4d7dbcd43ce9d6a5e81c
Halid Erdur <haliderdur@gmail.com>
2024-05-30 18:04:46 +0200

    practise

--- src/Replit/Replit_String/HiddenSentence.java
+++ src/Replit/Replit_String/HiddenSentence.java
@@ -1,4 +1,5 @@
 package Replit.Replit_String;
+
 /* Given a sentence which is ended with "." included letters, numbers, special charecters and then find hidden sentence.
     input:
          String sentence = "Y*78*8*o770987987u a869869=r????23425235e t54898)!'^h^^&%e b+%^^e%&+s%&
@@ -27,5 +28,22 @@ public class HiddenSentence {
         }
 
         System.out.println(temp);
+
+
+        String sentence2 = "Y*78*8*o770987987u a869869=r????23425235e t54898)!'^h^^&%e b+%^^e%&+s%&t g+%&%+u/%y&/s.&6756/D4o n556576ot5675 c657467a7657r7657e a6575b6575o5675u567t i5467t&";
+        StringBuilder temp2 = new StringBuilder();
+
+        for (int i = 0; i < sentence2.length(); i++) {
+            char currentChar = sentence2.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
+                temp2.append(currentChar);
+            } else if (currentChar == ' ') {
+                temp2.append(' ');
+            } else if (currentChar == '.') {
+                break;
+            }
+        }
+        System.out.println(temp2);
     }
 }
